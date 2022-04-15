ec2b880679be6815f1ab2dcc1c76196471395ba6
Leandro Felix <felix.dev2022@outlook.com>
2022-04-15 13:24:53 -0300

    Converte hora

--- meu_script.js
+++ meu_script.js
@@ -2,12 +2,26 @@
 let hours = document.getElementById('hours');
 let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
+let ampm = document.getElementById('ampm')
 
 //variaveis do relógio//
 let h = new Date().getHours();
 let m = new Date().getMinutes();
 let s = new Date().getSeconds();
+let am = h >= 12 ? "PM" : "AM";
+
+//converter 24hr em 12hr
+
+if(h > 12){
+    h = h - 12;
+}
+//adiciona zero antes digito
+
+h = (h < 10) ? "0" + h : h;
+m = (m < 10) ? "0" + m : m;
+s = (s < 10) ? "0" + s : s;
 
 hours.innerHTML = h;
 minutes.innerHTML = m;
 seconds.innerHTML = s;
+ampm.innerHTML = am;
